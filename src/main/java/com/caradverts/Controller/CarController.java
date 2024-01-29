943a9d90c9c7d99e762181135d348c4a969ba378
marinviskovic89 <marinviskovic89@gmail.com>
2024-01-29 13:36:47 +0100

    Modify erros

--- src/main/java/com/caradverts/Controller/CarController.java
+++ src/main/java/com/caradverts/Controller/CarController.java
@@ -23,25 +23,25 @@ public class CarController {
     @GetMapping("/cars")
     public ResponseEntity<List<Car>> getAllCars(@RequestParam(required = false) String sortby) {
         List<Car> cars;
-        String sortbyLower =  sortby.toLowerCase();
-        if(sortby == null) {
+        String sortbyLower = sortby.toLowerCase();
+        if (sortby == null) {
             cars = carRepository.findAll();
-        }
-        else {
+        } else {
             cars = carRepository.sortByParam(sortbyLower);
         }
-        if(cars.isEmpty()) {
+        if (cars.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<>(cars, HttpStatus.OK);
 
     }
+
     @GetMapping("/cars/{id}")
     public ResponseEntity<Car> getCarById(@PathVariable("id") long id) {
         try {
             Car car = carRepository.findById(id);
             return new ResponseEntity<>(car, HttpStatus.OK);
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             throw new ResponseStatusException(
                     HttpStatus.NOT_FOUND, "No car advert with given id was found.", ex);
         }
@@ -50,23 +50,24 @@ public class CarController {
     @PostMapping("/cars")
     public ResponseEntity<String> createCar(@RequestBody Car car) {
         try {
-            if(car.getPrice() >= 0) {
+            if (car.getPrice() >= 0 & car.getId() >= 0) {
                 carRepository.save(car);
                 return new ResponseEntity<>("Car was created successfully.", HttpStatus.CREATED);
-            }else
+            } else
                 return new ResponseEntity<>("Validation failed.", HttpStatus.UNPROCESSABLE_ENTITY);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (Exception ex) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "This is returned if json is invalid or cannot be parsed.", ex);
         }
     }
 
     @PutMapping("/cars/{id}")
-    public ResponseEntity<String> updateCar(@PathVariable("id") long id, @RequestBody Car c){
-        try{
-            if(c.getPrice() >= 0) {
+    public ResponseEntity<String> updateCar(@PathVariable("id") long id, @RequestBody Car c) {
+        try {
+            if (c.getPrice() >= 0 & c.getId() >= 0) {
                 Car existingCar = carRepository.findById(id);
                 Car newDetails = new Car();
-                if(existingCar!=null) {
+                if (existingCar != null) {
                     newDetails.setId(id);
                     newDetails.setTitle(c.getTitle());
                     newDetails.setFuelType(c.getFuelType());
@@ -78,26 +79,23 @@ public class CarController {
                     carRepository.update(newDetails);
                 }
                 return new ResponseEntity<>("Updated details successfully", HttpStatus.OK);
-            }
-            else
+            } else
                 return new ResponseEntity<>("Validation failed.", HttpStatus.UNPROCESSABLE_ENTITY);
-        }catch(Exception ex) {
-                return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (Exception ex) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "This is returned if a car advert with given id is not found.", ex);
         }
 
-}
+    }
 
     @RequestMapping(value = "/cars/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<String> deleteCar(@PathVariable("id") long id) {
-        try {
-            int result = carRepository.deleteById(id);
-            if (result == 0) {
-                return new ResponseEntity<>("Cannot find Car with id=" + id, HttpStatus.NOT_FOUND);
-            }
+        int res = carRepository.deleteById(id);
+        if (res == 1) {
             return new ResponseEntity<>("Car was deleted successfully.", HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>("Cannot delete Car.", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+
+    }
+    return new ResponseEntity<>("This is returned if a car advert with given id is not found.", HttpStatus.NOT_FOUND);
     }
 
 }
